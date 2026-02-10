5106380ad40a65f0a5942e4d5819f5d4e7650ae4
estherdomfeh213 <domfehesther2020@gmail.com>
2026-02-10 23:25:37 +0000

    feat: hands-on practice for variables in python

---
+++ chapter-2/2-2-simple_messages.py
@@ -0,0 +1,9 @@
+# Assign a message to a variable, and the print that message. Then change the value of the variable to a new message, and print the new message. 
+
+
+#message = "This is my first message"
+#print(message)
+
+# changed value of the variable
+message = "This is the new message"
+print(message)
---
+++ chapter-2/hello_world.py
@@ -0,0 +1,7 @@
+# Variables 
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world"
+print(message)
