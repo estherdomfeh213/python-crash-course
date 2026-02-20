c2f317adc4cc9ea8a0f252552d9bc7b79e0b56b2
estherdomfeh213 <domfehesther2020@gmail.com>
2026-02-20 15:48:49 +0000

    feat: guest  list and invitation message

---
+++ chapter-3/3-4-guest-list.py
@@ -0,0 +1,14 @@
+# If you could invite anyone, living or deceased, to dinner,
+# who would you invite? Make a list that includes at least three people you’d
+# like to invite to dinner. Then use your list to print a message to each person,
+# inviting them to dinner.
+
+
+#* List of people i did invite to dinner
+
+guest_list= ["agyei", "hanna", "edmund"]
+
+#* invitation message
+print(f"Hello {guest_list[0].title()}, consider this an ivitation to dinner tomorrow.")
+print(f"Hello {guest_list[1].title()}, consider this an ivitation to dinner tomorrow.")
+print(f"Hello {guest_list[2].title()}, consider this an ivitation to dinner tomorrow.")
