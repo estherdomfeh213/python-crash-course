821a0796640840b3e14da34cb5b71503cffa6bea
estherdomfeh213 <domfehesther2020@gmail.com>
2026-02-26 22:04:09 +0000

    doc:update chapter 4

---
+++ chapter-4/4--5-summing-a-million.py
@@ -0,0 +1,24 @@
+# Make a list of the numbers from one to one
+# million, and then use min() and max() to make sure your list actually starts at
+# one and ends at one million. Also, use the sum() function to see how quickly
+# Python can add a million numbers.
+
+
+one_millon_lists = []
+
+for value in range(1, 1000001):
+    million = value
+    one_millon_lists.append(value)
+
+#print(lists)
+maximum = max(one_millon_lists)
+minimum = min(one_millon_lists)
+summing = sum(one_millon_lists)
+print(minimum)
+print(maximum)
+print(summing)
+
+
+#* or summing 
+summing_1 = sum(range(1,1000001))
+print(summing_1)
