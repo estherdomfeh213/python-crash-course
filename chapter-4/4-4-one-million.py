cb9e4add762d13088e44bcabccd60af3747a81d8
estherdomfeh213 <domfehesther2020@gmail.com>
2026-02-26 21:36:24 +0000

    doc:update chapter 4

---
+++ chapter-4/4-4-one-million.py
@@ -0,0 +1,7 @@
+# Make a list of the numbers from one to one million, and
+# then use a for loop to print the numbers. (If the output is taking too long,
+# stop it by pressing CTRL-C or by closing the output window.)
+
+
+for million in range(1,1000000):
+    print(million)
